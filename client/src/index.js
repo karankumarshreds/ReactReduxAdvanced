28ea57d2ee8c9b75dead681e385b1757f1204302
karankumarshreds <naileditnerd@gmail.com>
2021-01-17 02:18:18 +0530

    add redux store

--- client/src/index.js
+++ client/src/index.js
@@ -3,4 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//redux
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
+import { Provider } from "react-redux";
+
+const store = createStore(
+  reducers,
+  {},
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
