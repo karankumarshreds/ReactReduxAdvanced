081cb59b133c7c6559b8994b4465ae47b23a0738
karankumarshreds <naileditnerd@gmail.com>
2021-01-16 20:02:08 +0530

    add order model

--- server/src/models/Order.ts
+++ server/src/models/Order.ts
@@ -1,11 +1,148 @@
 import mongoose from "mongoose";
 
-interface OrderAttrs {}
+interface OrderAttrs {
+  user: string;
+  orderItems: {
+    product: string;
+    qty: number;
+  }[];
+  shippingAddress: {
+    address: string;
+    city: string;
+    postalCode: string;
+    country: string;
+  };
+  paymentMethod: string;
+  paymentResult: {
+    id: string;
+    status: string;
+    update_time: string;
+    email_address: string;
+  };
+  taxPrice: number;
+  shippingPrice: number;
+  totalPrice: number;
+  isPaid: Boolean;
+  paidAt: Date;
+  isDelivered: Boolean;
+  deliveredAt: Date;
+  createdAt: Date;
+}
 
-interface OrderDoc extends mongoose.Document {}
+interface OrderDoc extends mongoose.Document {
+  user: string;
+  orderItems: {
+    product: string;
+    qty: number;
+  }[];
+  shippingAddress: {
+    address: string;
+    city: string;
+    postalCode: string;
+    country: string;
+  };
+  paymentMethod: string;
+  paymentResult: {
+    id: string;
+    status: string;
+    update_time: string;
+    email_address: string;
+  };
+  taxPrice: number;
+  shippingPrice: number;
+  totalPrice: number;
+  isPaid: Boolean;
+  paidAt: Date;
+  isDelivered: Boolean;
+  deliveredAt: Date;
+  createdAt: Date;
+}
 
 interface OrderModel extends mongoose.Model<OrderDoc> {
   build: (attrs: OrderAttrs) => OrderDoc;
 }
 
-const orderSchema = new mongoose.Schema({});
+const orderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  orderItems: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Product",
+      },
+      qty: { type: Number, default: 0 },
+    },
+  ],
+  shippingAddress: {
+    address: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    postalCode: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+  },
+  paymentMethod: {
+    type: String,
+    required: true,
+  },
+  // will be returned from the payment gateway
+  paymentResult: {
+    id: { type: String },
+    status: { type: String },
+    update_time: { type: String },
+    email_address: { type: String },
+  },
+  taxPrice: {
+    type: Number,
+    default: 0.0,
+  },
+  shippingPrice: {
+    type: Number,
+    default: 0.0,
+  },
+  totalPrice: {
+    type: Number,
+    default: 0.0,
+  },
+  isPaid: {
+    type: Boolean,
+    default: false,
+  },
+  paidAt: {
+    type: Date,
+  },
+  isDelivered: {
+    type: Boolean,
+    default: false,
+  },
+  deliveredAt: {
+    type: Date,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>("Order", orderSchema);
+
+export { Order };
