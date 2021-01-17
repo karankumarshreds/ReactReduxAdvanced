53c377f00d6e2201e8e8e614f6145b28b98bc655
karankumarshreds <naileditnerd@gmail.com>
2021-01-17 14:07:43 +0530

    add product reducer

--- client/src/reducers/productReducer.js
+++ client/src/reducers/productReducer.js
@@ -1,7 +1,23 @@
 import _ from "lodash";
+import {
+  PRODUCTS_LIST_FAILED,
+  PRODUCTS_LIST_REQUEST,
+  PRODUCTS_LIST_SUCCESS,
+} from "../types";
 
 export const productReducer = (state = {}, action) => {
   switch (action.type) {
+    case PRODUCTS_LIST_REQUEST:
+      return { ...state, loading: true, products: {} };
+    case PRODUCTS_LIST_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        // change array to object with id as keys
+        products: { ..._.mapKeys(action.payload, id) },
+      };
+    case PRODUCTS_LIST_FAILED:
+      return { loading: false, error: action.payload };
     default:
       return state;
   }
