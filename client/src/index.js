2c148d72b8685a0631cf57f288bae8c1c2603d8e
karankumarshreds <naileditnerd@gmail.com>
2021-01-19 04:33:01 +0530

    refactor initial state

--- client/src/index.js
+++ client/src/index.js
@@ -11,13 +11,15 @@ import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import reducers from "./reducers";
 
+const INITIAL_STATE = {
+  // set the cartItems value in case its saved in the browser
+  cartState: {
+    cartItems: JSON.parse(localStorage.getItem("cartItems")) || [],
+  },
+};
 const store = createStore(
   reducers,
-  {
-    cartState: {
-      cartItems: JSON.parse(localStorage.getItem("cartItems")) || [],
-    },
-  },
+  INITIAL_STATE,
   composeWithDevTools(applyMiddleware(thunk))
 );
 
