e2f85e1d086d091f09fd7fd317a0c79d6078ed29
karankumarshreds <naileditnerd@gmail.com>
2021-01-27 22:12:19 +0530

    add signin route

--- server/src/controllers/user-controllers.ts
+++ server/src/controllers/user-controllers.ts
@@ -1,8 +1,27 @@
 import { Request, Response } from "express";
 import { generateToken } from "../utils/jwt";
+import { Password } from "../utils/password";
+// models
+import { User } from "../models/User";
+import { BadRequestError } from "../utils";
 
 const signinUser = async (req: Request, res: Response) => {
-  res.send({ OK: "OK" });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new BadRequestError("Invalid username or password");
+  }
+  const validPassword = await Password.compare(user.password, password);
+  if (!validPassword) {
+    throw new BadRequestError("Invalid username or password");
+  }
+  res.json({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    isAdmin: user.isAdmin,
+    token: generateToken(user.id),
+  });
 };
 
 export { signinUser };
