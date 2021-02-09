e635e6ba1306faf471a91012de964a9df4a2b4c3
karankumarshreds <naileditnerd@gmail.com>
2021-02-09 22:30:04 +0530

    add profile page

---
+++ client/src/screens/ProfileScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect, useDispatch } from 'react-redux';
+import { profileFetchAction } from '../actions/profileActions';
+import { Container } from 'react-bootstrap';
+
+const ProfileScreen = (props) => {
+  const dispatch = useDispatch();
+  useEffect = () => {
+    dispatch(profileFetchAction());
+  };
+  console.log(props.profileState, 'PROFILE STATE');
+  return <Container>Profile Page</Container>;
+};
+
+const mapStateToProps = ({ profileState }) => {
+  return { profileState };
+};
+
+export default connect(mapStateToProps)(ProfileScreen);
